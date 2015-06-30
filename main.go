2cb4411cc8e964fd3ba804e3264e9a0b87a6c4a3
vzever <vzever@users.noreply.github.com>
2015-06-30 16:39:23 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func checkerr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func read(finame string) string {
+	fi, err := os.Open(finame)
+	checkerr(err)
+	defer fi.Close()
+	b := make([]byte, 4)
+	_, err = fi.ReadAt(b, 60)
+	checkerr(err)
+	offset := int(b[0]) + int(b[1])*256 + int(b[2])*256*256 + int(b[3])*256*256*256
+	value := make([]byte, 2)
+	_, err = fi.ReadAt(value, int64(offset)+4)
+	checkerr(err)
+	return string(value)
+
+}
+
+func main() {
+	flag.Parse()
+	finame := flag.Arg(0)
+	value := read(finame)
+	switch value {
+	case string([]byte{100, 134}):
+		fmt.Println("amd64")
+	case string([]byte{76, 1}):
+		fmt.Println("win32")
+	default:
+		fmt.Println("not PE program")
+		fmt.Println([]byte(value))
+	}
+}
